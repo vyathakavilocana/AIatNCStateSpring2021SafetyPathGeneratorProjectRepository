5acc2863146df48607a1ed52ee3e000ec683e61a
vyathakavilocana <prathammc@gmail.com>
2021-04-13 00:52:10 -0400

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This Spring 2021 AI at NC State project repository cotnains all of the machine learning prototyping code for the Safety Path Generator project. Acquiring tabular de-identified crime data from local college cities in North and South Carolina, [C[C[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[A[Do[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[cities in North and South Carolina, it seeks to harness the power of applied AI models to provide a heat map at any given time and place where incidences of crime per type are occuring and alert users of those areas, thereby providing with a path to take to avoid certains areas at certain points of the day where their likelihood of becoming victims of a particular type of crime are higher.',
+    author='AI at NC State ()[D [DPratham Chhabria, Swathi Dinkaran, [C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[CSrisheel Gunnisetti) and Clemson AI Club (Jeremy Wang, Jeremy Spooner)',
+    license='MIT',
+)
